322bf1f9adccf3cc1f96dac8c5be1d87184a303e
HN <nhai.qn@gmail.com>
2017-04-16 01:19:20 -0400

    try

--- devtools/ci/load_functions.sh
+++ devtools/ci/load_functions.sh
@@ -2,83 +2,58 @@
 
 url="http://ambermd.org/downloads/ambertools-dev/AmberTools17.tar.gz"
 tarfile=`python -c "url='$url'; print(url.split('/')[-1])"`
-version='16'
+if [ "$CONDA" = "True" ]; then
+    ambertools_binary_url='https://480-81537431-gh.circle-artifacts.com/0/tmp/circle-artifacts.SHoEmLc/ambertools-build/amber-conda-bld/linux-64/ambertools-17.0-0.tar.bz2'
+else
+    ambertools_binary_url='https://480-81537431-gh.circle-artifacts.com/0/tmp/circle-artifacts.SHoEmLc/ambertools-build/amber-conda-bld/non-conda-install/linux-64.ambertools-17.0-0.16Apr17.H0417.tar.bz2'
+fi
+binar_tarfile=`python -c "url='${ambertools_binary_url}'; print(url.split('/')[-1])"`
+amber_version='16'
+ambertools_version='17'
 
-function download_ambertools(){
+
+function install_python(){
+    set -ex
+    if [ "$PYTHON_VERSION" = "2.7" ]; then
+        bash $HOME/source_code/AmberTools/src/configure_python --prefix $HOME
+        export PATH=$HOME/miniconda/bin:$PATH
+    elif [ "$PYTHON_VERSION" = "3.5" ]; then
+        bash $HOME/source_code/AmberTools/src/configure_python --prefix $HOME -v 3
+        export PATH=$HOME/miniconda/bin:$PATH
+    fi
+}
+
+
+function setup_ambertools(){
+    cwd=`pwd`
+    mkirt $HOME/source_code
+    cd $HOME/source_code
     wget $url -O $tarfile
     tar -xf $tarfile
-}
+    cd $cwd
+    install_python
 
-function install_ambertools_travis(){
-    set -ex
-    # This AmberTools version is not an official release. It is meant for testing.
-    # DO NOT USE IT PLEASE.
-    osname=`python -c 'import sys; print(sys.platform)'`
-    cd amber$version
-    if [ $osname = "darwin" ]; then
-        unset CC CXX
-        compiler="-macAccelerate clang"
+    wget ${ambertools_binary_url} -O ${binary_tarfile}
+    if [ "$CONDA" = "True" ]; then
+        conda install ${binary_tarfile}
     else
-        compiler="gnu"
+        tar -xf ${binary_tarfile}
     fi
-    if [ "$MINICONDA_WILL_BE_INSTALLED" = "True" ]; then
-        yes | ./configure $compiler
-    elif [ "$MINICONDA_IN_AMBERHOME" = "True" ]; then
-        bash AmberTools/src/configure_python --prefix `pwd`
-        ./configure $compiler
-    elif [ "$USE_AMBER_PREFIX" = "True" ]; then
-        mkdir $HOME/TMP/
-        yes | ./configure --prefix $HOME/TMP $compiler
-    elif [ "$USE_WITH_PYTHON" = "True" ]; then
-        bash AmberTools/src/configure_python --prefix $HOME
-        export PATH=$HOME/miniconda/bin:$PATH
-        ./configure --with-python $HOME/miniconda/bin/python $compiler
-    elif [ "$SKIP_PYTHON" = "True" ]; then
-        ./configure --skip-python $compiler
-    elif [ "$AMBER_INSTALL_MPI" = "True" ]; then
-        yes | ./configure $compiler
-        make install -j2
-        ./configure -mpi $compiler # will do make install later
-    elif [ "$PYTHON_VERSION" = "3.5" ]; then
-        bash AmberTools/src/configure_python --prefix $HOME -v 3
-        export PATH=$HOME/miniconda/bin:$PATH
-        ./configure --with-python $HOME/miniconda/bin/python $compiler
-    fi
-    
-    make install -j2
 }
 
-function install_ambertools_circleci(){
-    mkdir $HOME/TMP
-    cd $HOME/TMP
-    python $HOME/ambertools-test/amber$version/AmberTools/src/conda_tools/build_all.py --exclude-osx --sudo --date
+
+function setup_ambertools_circleci(){
+    echo "Nothing"
 }
 
-function run_long_test_simplified(){
-    # not running all tests, skip any long long test.
-    cd $AMBERHOME/AmberTools/test
-    python $TRAVIS_BUILD_DIR/devtools/ci/ci_test.py $TEST_TASK
-    # python $TRAVIS_BUILD_DIR/amber$version/AmberTools/src/conda_tools/amber.run_tests $TEST_TASK
-}
 
 function run_tests(){
-    set -ex
-    if [ "$USE_AMBER_PREFIX" = "True" ]; then
-        source $HOME/TMP/amber.sh
-        ls $AMBERHOME
-        ls $HOME/TMP/
-        ls $HOME/TMP/*/
+    if [ "$CONDA" = "True" ]; then
+        export AMBERHOME=`python -c "import sys; print(sys.prefix)"`
     else
-        source $TRAVIS_BUILD_DIR/amber$version/amber.sh
+        source $HOME/amber${ambertools_version}/amber.sh
     fi
-    if [ "$TEST_TASK" != "" ]; then
-        run_long_test_simplified
-    else
-        if [ "$SKIP_PYTHON" != "True" ]; then
-            cat $TRAVIS_BUILD_DIR/amber$version/AmberTools/src/conda-recipe/run_test.sh | sed "s/python/amber.python/g" > $HOME/run_test.sh
-            bash $HOME/run_test.sh
-        else
-            (cd $AMBERHOME/AmberTools/test && make test.ambermini)
-        fi
-    fi
+    amber.setup_test_folders $HOME/source_code/amber${amber_version}
+    python $TRAVIS_BUILD_DIR/devtools/ci/ci_test.py $TEST_TASK
+    # python $TRAVIS_BUILD_DIR/amber$version/AmberTools/src/conda_tools/amber.run_tests $TEST_TASK
 }
