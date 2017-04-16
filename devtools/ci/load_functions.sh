df1626f5999536920c4e8bd0815b9acd1c4e6123
HN <nhai.qn@gmail.com>
2017-04-16 01:29:14 -0400

    ack

--- devtools/ci/load_functions.sh
+++ devtools/ci/load_functions.sh
@@ -15,10 +15,10 @@ ambertools_version='17'
 function install_python(){
     set -ex
     if [ "$PYTHON_VERSION" = "2.7" ]; then
-        bash $HOME/source_code/amber${amber_version}AmberTools/src/configure_python --prefix $HOME
+        bash $HOME/source_code/amber${amber_version}/AmberTools/src/configure_python --prefix $HOME
         export PATH=$HOME/miniconda/bin:$PATH
     else
-        bash $HOME/source_code/amber${amber_version}AmberTools/src/configure_python --prefix $HOME -v 3
+        bash $HOME/source_code/amber${amber_version}/AmberTools/src/configure_python --prefix $HOME -v 3
         export PATH=$HOME/miniconda/bin:$PATH
     fi
 }
