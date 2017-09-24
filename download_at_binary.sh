153eff77b6e29938d636583a35c5b386dc3c61ff
HN <nhai.qn@gmail.com>
2017-09-23 23:01:13 -0400

    download

--- download_at_binary.sh
+++ download_at_binary.sh
@@ -1,6 +1,7 @@
-wget https://github.com/hainm/ambertools-dev-binary/blob/master/tmp_osx_ATaa?raw=true -O $HOME/tmp_osx_ATab
-wget https://github.com/hainm/ambertools-dev-binary/blob/master/tmp_osx_ATab?raw=true -O $HOME/tmp_osx_ATab
-wget https://github.com/hainm/ambertools-dev-binary/blob/master/tmp_osx_ATac?raw=true -O $HOME/tmp_osx_ATac
+wget https://github.com/hainm/ambertools-dev-binary/blob/master/tmp_osx_ATaa?raw=true -O $HOME/tmp_osx_ATab > log
+wget https://github.com/hainm/ambertools-dev-binary/blob/master/tmp_osx_ATab?raw=true -O $HOME/tmp_osx_ATab > log
+wget https://github.com/hainm/ambertools-dev-binary/blob/master/tmp_osx_ATac?raw=true -O $HOME/tmp_osx_ATac > log
+tail -50 log
 cat $HOME/tmp_osx_AT* > $HOME/AT.tar
 cd $HOME
 tar -xf AT.tar
